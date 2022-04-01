8d10f488d1c25a2a2b61da520909f3d5cc308d4f
Galantini-L <galantini99@gmail.com>
2022-04-01 13:00:12 -0300

    add functionalities and fix features

--- models.py
+++ models.py
@@ -1,19 +1,17 @@
 from datetime import datetime
+from email.policy import default
 from sqlalchemy import Column, Boolean, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.sqltypes import DateTime
-from datetime import datetime
 from db import engine
-from sqlalchemy.orm import sessionmaker
 
-#Para crear un model la clase "taskitem" debe heredar esta clase
+#Para crear un model la clase "taskItem" debe heredar esta clase
 Base = declarative_base()
 
 class tasksItems(Base):
     __tablename__ = 'tasks'
-    id = Column(Integer(),primary_key=True)
+    id = Column(Integer,primary_key=True)
     name = Column(String(60), nullable=False)
-    compleate = Column(Boolean,default=False)
+    compleate = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.now())
-    
+
 Base.metadata.create_all(engine)
--- views.py
+++ views.py
@@ -1,8 +1,10 @@
+from functools import update_wrapper
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask.helpers import flash
-from sqlalchemy.sql.schema import Index
 from session import session
 from models import tasksItems
+from sqlalchemy import update
+
 
 views = Blueprint(__name__,"views")
 
@@ -11,24 +13,37 @@ def home():
     tasks = session.query(tasksItems).all()
     return render_template("home.html", tasks = tasks)
 
-@views.route ("/list")
-def list():
-    return render_template("list.html")
-
 @views.route("/add", methods = ['POST'])
-
 def add():
     if request.method == 'POST':
         if request.form['task'] != '':
-            task= tasksItems(name=request.form['task'],compleate=False)
+            task= tasksItems(name=request.form['task'])
             session.add(task)
             session.commit()
-            flash('New item has been added succesfuly')
+            flash('New item has been added succesfuly', "success")
             return redirect(url_for('views.home'))
             
         else:
-            flash('You can`t add an empty task')
+            flash('You can`t add an empty task', "warning")
             return redirect(url_for('views.home'))
 
-            
+@views.get("/update/<int:tasks_id>")
+def update(tasks_id):
+    query1 = session.query(tasksItems).filter(tasksItems.id == tasks_id)
+    for q in query1:
+        q.compleate = not q.compleate
+    session.commit()
 
+
+    return redirect(url_for('views.home'))
+            
+@views.get("/delete/<int:tasks_id>")
+def delete(tasks_id):
+    session.query(tasksItems).filter(
+        tasksItems.id == tasks_id).delete()
+    session.commit()
+        
+    return redirect(url_for('views.home'))
+
+
+    #task_list = request.form.getlist('task')
