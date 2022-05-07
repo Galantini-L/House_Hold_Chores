9da9001d546ded6b8b60d1846c4f9fc71ad6e836
Galantini-L <galantini99@gmail.com>
2022-05-07 02:54:19 -0300

    more fixing

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
-from session import session
+from flask import Flask, session
 from flask import render_template, request, redirect, url_for
+from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from sqlalchemy import Column, Boolean, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from flask.helpers import flash
 import secrets
-from sqlalchemy.orm.session import sessionmaker
 from db import engine
 
 
@@ -26,15 +25,18 @@ class tasksItems(declarative_base()):
     created_at = Column(DateTime, default=datetime.now())
 
 
+Session = sessionmaker(engine)
+
+
 
 @app.route("/")
 def home():
     try:
-        Session = sessionmaker(engine)
-        session = Session()
-        tasks = session.query(tasksItems).all()
-        return render_template("home.html", tasks = tasks)
-
+        with engine.connect() as connection:
+            with Session(bind=connection) as session:
+                tasks = session.query(tasksItems).all()
+                session.close()
+                return render_template("home.html", tasks = tasks)
     except:
         print('-------------Error whit the Server-------------')
         flash('Error Conectiong whit the server', "danger")
@@ -43,34 +45,43 @@ def home():
 @app.route("/add", methods = ['POST'])
 def add():
     if request.method == 'POST':
-        if request.form['task'] != '':
-            task= tasksItems(name=request.form['task'])
-            session.add(task)
-            session.commit()
-            flash('New item has been added succesfuly', "success")
-            return redirect(url_for('home'))
-            
-        else:
-            flash('You can`t add an empty task', "warning")
-            return redirect(url_for('home'))
+        with engine.connect() as connection:
+            with Session(bind=connection) as session:
+                if request.form['task'] != '':
+                    task= tasksItems(name=request.form['task'])
+                    session.add(task)
+                    session.commit()
+                    session.close()
+                    flash('New item has been added succesfuly', "success")
+                    return redirect(url_for('home'))
+                    
+                else:
+                    flash('You can`t add an empty task', "warning")
+                    return redirect(url_for('home'))
 
 @app.get("/update/<int:tasks_id>")
 def update(tasks_id):
-    query1 = session.query(tasksItems).filter(tasksItems.id == tasks_id)
-    for q in query1:
-        q.compleate = not q.compleate
-    session.commit()
+        with engine.connect() as connection:
+            with Session(bind=connection) as session:
+                query1 = session.query(tasksItems).filter(tasksItems.id == tasks_id)
+                for q in query1:
+                    q.compleate = not q.compleate
+                session.commit()
+                session.close()
 
 
-    return redirect(url_for('home'))
-            
+                return redirect(url_for('home'))
+                        
 @app.get("/delete/<int:tasks_id>")
 def delete(tasks_id):
-    session.query(tasksItems).filter(
-        tasksItems.id == tasks_id).delete()
-    session.commit()
-        
-    return redirect(url_for('home'))
+        with engine.connect() as connection:
+            with Session(bind=connection) as session:
+                session.query(tasksItems).filter(
+                    tasksItems.id == tasks_id).delete()
+                session.commit()
+                session.close()
+                    
+                return redirect(url_for('home'))
 
 
     #task_list = request.form.getlist('task')
@@ -78,4 +89,4 @@ def delete(tasks_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8020)
+    app.run()
